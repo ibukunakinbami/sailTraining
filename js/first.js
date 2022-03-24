b3579a9c9b514375c9950eebc7c61c9440d2d15c
Ibukun Akinbami <ibukunakinbami@gmail.com>
2022-03-24 14:57:36 +0100

    Commit first Javascript code

---
+++ js/first.js
@@ -0,0 +1,64 @@
+var greetings ="Hello World!";
+console.log(greetings)
+// console.log(greetings)
+// console.log(greetings)
+console.log("")
+
+var  sail ="Developers Cabal"
+console.log("")
+
+console.log(sail)
+console.log("")
+
+var status = "Watching Netflix";
+myStatus = "Relaxing at the Beach";
+console.log(myStatus)
+console.log("")
+
+var myDressing = "so modest";
+myDressing = "High Class";
+console.log(myDressing)
+console.log("")
+
+/* Variables give the values of other variables */
+var defaultStatus = "Hi There";
+var status = "Playing Football"
+newStatus = defaultStatus;
+console.log(newStatus)
+console.log("")
+
+// Example 1
+var campus = "Senate Building";
+var church = "Mission House";
+church = campus;
+console.log(church)
+console.log("")
+
+// Example 2
+var temrperature = "100 degree Celcius";
+var temrperature = "0 degree Celcius";
+console.log(temrperature)
+console.log("")
+
+// Variable - Numbers
+var facebookLikes = 12,
+facebookLikes = 100;
+console.log(facebookLikes)
+console.log("")
+
+// Example 1
+var instagramLikes = 200;
+console.log(instagramLikes + facebookLikes + 400)
+console.log("")
+
+// Example 2
+var myCalculator = 3;
+myCalculator =  4;
+console.log(myCalculator/2)
+console.log("")
+
+
+
+
+
+
