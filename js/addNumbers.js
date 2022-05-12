fdd54fe1a66900abb9000e00f7a78ff803009ba8
Ibukun Akinbami <ibukunakinbami@gmail.com>
2022-05-12 07:00:44 +0100

    Updated js and css file

--- js/addNumbers.js
+++ js/addNumbers.js
@@ -1,25 +1,10 @@
-// let num1 = parseInt(prompt("Enter first Number"));
-// let num2 = parseInt(prompt("Enter second Number"));
+// Declaration Block: 
+let num1 = parseInt(prompt("Enter first Number"));
+let num2 = parseInt(prompt("Enter second Number"));
+const addOperator = document.querySelector("#addNumbers");
 
-function addNumbers(){
-    let result = num1 + num2;
-    return result
-    //console.log(result)
-    // to check type of result data type
-    //console.log(typeof(result))
-    // document.querySelector("#output").textContent = result;
-}
-
-
-
-addNumbers();
-
-
-//button
-// document.querySelector("#addNumbers")
-
-//
-
-// addNumbers();
-
-
+// Function Block: Addition of two numbers
+addOperator.addEventListener("click", function () {
+    let sum = num1 + num2;
+    document.querySelector("#output").textContent = sum;
+})
