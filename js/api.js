7f06f4a3f697a5ee50a26dd0cf80bbf218d63b58
Ibukun Akinbami <ibukunakinbami@gmail.com>
2022-05-24 11:51:14 +0100

    Updated api get file

--- js/api.js
+++ js/api.js
@@ -1,3 +1,4 @@
+// GET DATA
 const getBtn = document.querySelector("#getReq")
 
 const getData =() => {
@@ -8,4 +9,16 @@ const getData =() => {
 }
 
 getBtn.addEventListener("click",getData)
+
+// POST DATA
+const postBtn = document.querySelector("#postReq")
+
+const postData =() => {
+    fetch("https://reqres.in/api/users?page=2").then (b => {
+    //  return b.json;
+     console.log(b);
+    })
+}
+
+postBtn.addEventListener("click",postData)
 
