1ed97a14fd5703931b4b2174e4c5bf110cfb3e26
Ibukun Akinbami <ibukunakinbami@gmail.com>
2022-05-31 14:10:40 +0100

    Updated regForm

--- js/regForm.js
+++ js/regForm.js
@@ -1,1 +1,15 @@
-const form = document.querySelector(#form)
+const form = document.querySelector(#form);
+form
+
+
+
+// fecth data
+fetch("http://httpbin.org/post",{
+ method: "POST",
+ body: payload,
+})
+    .then(res=>res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+})
